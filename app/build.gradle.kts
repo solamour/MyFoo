13ab70fcc4187bc1c7e97dbf4a845a386015df3e
solamour <solamour@gmail.com>
2023-08-26 13:42:30 -0700

    Update dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -57,11 +57,11 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.core:core-ktx:1.10.1")         // targetSdk = 32:("1.7.0")
+    implementation("androidx.core:core-ktx:1.10.1")         // "targetSdk = 32" -> "1.7.0".
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 
-    implementation(platform("androidx.compose:compose-bom:2023.05.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
