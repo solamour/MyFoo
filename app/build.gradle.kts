5b00b8aa89c6c17b97dd04631e56696648efe888
solamour <solamour@gmail.com>
2026-06-16 21:55:48 -0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.util.Properties
 
 plugins {
     alias(libs.plugins.com.android.application)
-    //alias(libs.plugins.org.jetbrains.kotlin.android)
     alias(libs.plugins.org.jetbrains.kotlin.plugin.compose)
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,9 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "MyFoo"
 include(":app")
